0514c5a355bd01fe381bd79a9ca0ff55b290763a
Dvanegas067 <47580724+Dvanegas067@users.noreply.github.com>
2020-08-30 18:35:11 -0500

    Add files via upload

---
+++ funcionpendulo.java
@@ -0,0 +1,32 @@
+
+package pendulo;
+
+
+
+public class funcionpendulo {
+    
+    public static void main(String[] args) {
+        Pendulo a = new Pendulo();   
+        a.datos();  
+        a.oscilacion(); 
+        System.out.println(a.getL());
+        System.out.println(a.getA());
+                
+        
+        
+        
+                
+        
+        
+       
+       
+
+
+        
+
+    }
+
+}
+
+    
+
